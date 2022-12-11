bdfdd9cedc0ea3625ee88a87267430ae3922ec41
Aleksandar Petrushev <aleksandarpetrushev27@gmail.com>
2022-12-11 17:16:32 +0100

    Add Controller and Views for Subscriber

--- app/controllers/rails_webhooks/subscribers_controller.rb
+++ app/controllers/rails_webhooks/subscribers_controller.rb
@@ -1,17 +1,60 @@
 module RailsWebhooks
   class SubscribersController < ApplicationController
+    before_action :set_subscriber, only: %i[ show edit update destroy ]
+
+    # GET /subscribers
     def index
-      @subscribers = RailsWebhooks::Subscriber.all
+      @subscribers = Subscriber.all
     end
 
-    def show; end
+    # GET /subscribers/1
+    def show
+    end
 
-    def new; end
+    # GET /subscribers/new
+    def new
+      @subscriber = Subscriber.new
+    end
 
-    def edit; end
+    # GET /subscribers/1/edit
+    def edit
+    end
 
-    def update; end
+    # POST /subscribers
+    def create
+      @subscriber = Subscriber.new(subscriber_params)
 
-    def destroy; end
+      if @subscriber.save
+        redirect_to @subscriber, notice: "Subscriber was successfully created."
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    # PATCH/PUT /subscribers/1
+    def update
+      if @subscriber.update(subscriber_params)
+        redirect_to @subscriber, notice: "Subscriber was successfully updated."
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    # DELETE /subscribers/1
+    def destroy
+      @subscriber.destroy
+      redirect_to subscribers_url, notice: "Subscriber was successfully destroyed."
+    end
+
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_subscriber
+        @subscriber = Subscriber.find(params[:id])
+      end
+
+      # Only allow a list of trusted parameters through.
+      def subscriber_params
+        params.require(:subscriber).permit(:url, :active, :subscriptions)
+      end
   end
 end
